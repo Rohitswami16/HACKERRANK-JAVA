103289cb53e787110c48dd5830b95c1d131f8615
Rohit Swami <121743390+Rohitswami16@users.noreply.github.com>
2025-01-16 13:59:24 +0400

    Java List

--- 4-Data-Structure-06-Java-List.java
+++ 4-Data-Structure-06-Java-List.java
@@ -5,22 +5,15 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
-
-      
         int n = sc.nextInt();
         List<Integer> list = new ArrayList<>();
-
         for (int i=0; i<n; i++) {
             list.add(sc.nextInt());
         }
-
         int q = sc.nextInt();
-
-       
         for (int i = 0; i < q; i++) {
             String str = sc.next();
             if (str.equals("Insert")) {
