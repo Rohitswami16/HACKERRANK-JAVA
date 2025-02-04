0b73545698c45781853883ebf16b3a18722b4813
Rohit Swami <121743390+Rohitswami16@users.noreply.github.com>
2025-02-04 16:47:10 +0530

    Update 1-Intro-01-welcome-to-java.java

--- 1-Intro-01-welcome-to-java.java
+++ 1-Intro-01-welcome-to-java.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         System.out.println("Hello, World.");
